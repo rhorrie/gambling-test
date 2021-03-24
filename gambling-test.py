ae3ea469cae2650b010f2df68834ecb1a7977fad
rhorrie <57199828+rhorrie@users.noreply.github.com>
2021-03-24 14:48:32 -0500

    Update gambling-test.py

--- gambling-test.py
+++ gambling-test.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    
+    df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                       'B': [5, 6, 7, 8, 9],
+                       'C': ['a', 'b', 'c--', 'd', 'e']})
+    
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+    #return "<h1>Welcome to our server !!</h1>"
