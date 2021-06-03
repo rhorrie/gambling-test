1a0e5958b7184e29f8d5f8c97d794f17d7fa0fe5
Ryan Horrie <rhorrie13@gmail.com>
2021-06-03 15:20:15 -0500

    Updating to have less of a time frame so it does not timeout.

--- clock.py
+++ clock.py
@@ -1,9 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from rq import Queue
+from worker import conn
+
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
+q = Queue(connection=conn)
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=2)
 def scheduled_job():
-	execfile('update_table_daily.py')
+	q.enqueue(execfile('update_table_daily.py'))
 
 sched.start()
--- create_table.py
+++ create_table.py
@@ -9,13 +9,16 @@ import pickle
 
 app = Flask(__name__)
 
-datelist = pd.date_range(start="2021-04-01", end=(datetime.datetime.today() - datetime.timedelta(days=1)))
-team_dict = get_full_team_array(datelist)
+#datelist = pd.date_range(start="2021-04-01", end=(datetime.datetime.today() - datetime.timedelta(days=1)))
+#team_dict = get_full_team_array(datelist)
 
-file = open("data.pkl", "wb")
-pickle.dump(team_dict, file)
-file.close()
+#file = open("data.pkl", "wb")
+#pickle.dump(team_dict, file)
+#file.close()
 
+#df = pd.DataFrame.from_dict(team_dict)
+
+team_dict = {"Name":[], "Wins":[], "Losses":[], "Home Wins":[], "Home Losses":[], "Away Wins":[], "Away Losses":[]}
 df = pd.DataFrame.from_dict(team_dict)
 
 @app.route('/')
@@ -25,3 +28,7 @@ def index():
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+
+#Taking too long to create table in heroku. Workers are timing out. Look at this link https://bigishdata.com/2016/12/15/running-python-background-jobs-with-heroku/
+# to possibly gain a better understanding of having a worker.py do the webscraping in the background so there is no worry of timing out.
