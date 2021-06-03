2ccd58a3afcb7b34293cee50458e44143109411d
Ryan Horrie <rhorrie13@gmail.com>
2021-06-03 15:55:51 -0500

    Testing again

--- clock.py
+++ clock.py
@@ -2,11 +2,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-<<<<<<< HEAD
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=2)
-=======
-@sched.scheduled_job('cron', day_of_week='mon-sat', hour=2)
->>>>>>> 2c195d6cf677fa2ed4fb37624e964e7aeb9b6978
+
 def scheduled_job():
 	execfile('update_table_daily.py')
 
--- create_table.py
+++ create_table.py
@@ -9,17 +9,13 @@ import pickle
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 datelist = pd.date_range(start="2021-05-20", end=(datetime.datetime.today() - datetime.timedelta(days=1)))
 team_dict = get_full_team_array(datelist)
-=======
-#datelist = pd.date_range(start="2021-05-05", end=(datetime.datetime.today() - datetime.timedelta(days=1)))
-#team_dict = get_full_team_array(datelist)
->>>>>>> 2c195d6cf677fa2ed4fb37624e964e7aeb9b6978
 
-#file = open("data.pkl", "wb")
-#pickle.dump(team_dict, file)
-#file.close()
+
+file = open("data.pkl", "wb")
+pickle.dump(team_dict, file)
+file.close()
 
 team_dict = pickle.load(open("data.pkl", "rb"))
 
@@ -32,10 +28,6 @@ def index():
 
 if __name__ == '__main__':
 	app.run(debug=True)
-<<<<<<< HEAD
-
 
 #Taking too long to create table in heroku. Workers are timing out. Look at this link https://bigishdata.com/2016/12/15/running-python-background-jobs-with-heroku/
 # to possibly gain a better understanding of having a worker.py do the webscraping in the background so there is no worry of timing out.
-=======
->>>>>>> 2c195d6cf677fa2ed4fb37624e964e7aeb9b6978
